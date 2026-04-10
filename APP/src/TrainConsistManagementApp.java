f338cb8323201aaba5deda33ab248b90e9655283
BHARATH5059 <mopadabharath5059@gmail.com>
2026-04-10 14:40:05 +0530

    Refactor TrainConsistManagementApp and remove RoomInventory
    Removed RoomInventory class and integrated its functionality into TrainConsistManagementApp. Added unique bogie IDs display.

--- APP/src/TrainConsistManagementApp.java
+++ APP/src/TrainConsistManagementApp.java
@@ -1,44 +1,20 @@
 import java.util.*;
 
-class RoomInventory {
-    private HashMap<String, Integer> inventory;
-
-    public RoomInventory() {
-        inventory = new HashMap<>();
-        inventory.put("Single", 10);
-        inventory.put("Double", 5);
-        inventory.put("Suite", 2);
-    }
-
-    public int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
-    }
-
-    public void updateAvailability(String roomType, int count) {
-        inventory.put(roomType, count);
-    }
-
-    public void displayInventory() {
-        System.out.println("Current Room Inventory:");
-        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
-    }
-}
-
-public class TrainConsistManagementApp {
+public class TrainConsistManagemaentApp {
     public static void main(String[] args) {
-        System.out.println("=== Book My Stay App ===");
+        System.out.println("=== Train Consist Management App ===");
 
-        RoomInventory inventory = new RoomInventory();
+        Set<String> bogieIds = new HashSet<>();
 
-        inventory.displayInventory();
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
 
-        inventory.updateAvailability("Single", 8);
-
-        System.out.println("After Update:");
-        inventory.displayInventory();
-
-        System.out.println("Available Double Rooms: " + inventory.getAvailability("Double"));
+        System.out.println("Unique Bogie IDs:");
+        for (String id : bogieIds) {
+            System.out.println(id);
+        }
     }
 }
